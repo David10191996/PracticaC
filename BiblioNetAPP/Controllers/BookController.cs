df23b176a17349e42a0a1e1a4bc36416b9056229
David10191996 <David.10191996@gmail.com>
2022-03-17 11:07:13 -0500

    primer commit

---
+++ BiblioNetAPP/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using BiblioNetAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiblioNetAPP.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult RegisterBook()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterBook(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(book);
+            }
+            return View();
+        }
+    }
+}
---
+++ BiblioNetAPP/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BiblioNetAPP.Models
+{
+    public class Book
+    {
+
+        [Required(ErrorMessage = "{0} is required")]
+        public string BookName { get; set; }
+
+        public string Author { get; set; }
+
+        public int Price { get; set; }
+    }
+}
